e54c00707c27c91de71c51a1322889162b07b46d
MikPhD <mik.quattromini@gmail.com>
2023-10-24 16:46:41 +0200

    try

--- app.py
+++ app.py
@@ -1,35 +1,52 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
-# from flask_sqlalchemy import SQLAlchemy
-# from sqlalchemy import create_engine, inspect, insert
+import os
+
+from flask import (Flask, redirect, render_template, request,
+                   send_from_directory, url_for)
 
 
 app = Flask(__name__)
 
-used_ids = set(["prova"])
-
 @app.route('/')
 def index():
+    print('Request for index page received')
     return render_template('index.html')
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    input_value = request.form.get('id')
-    print("Valore del campo di input:", input_value)
-    return redirect(url_for('success'))
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/check_id', methods=['POST'])
-def check_id():
-    input_value = request.form.get('id')
+# @app.route('/submit', methods=['POST'])
+# def submit():
+#     input_value = request.form.get('id')
+#     print("Valore del campo di input:", input_value)
+#     return redirect(url_for('success'))
+#
+# @app.route('/check_id', methods=['POST'])
+# def check_id():
+#     input_value = request.form.get('id')
+#
+#     if input_value in used_ids:
+#         return jsonify({'valid': False})
+#     else:
+#         return jsonify({'valid': True})
+#
+#
+# @app.route('/success')
+# def success():
+#     return render_template('success.html')
 
-    if input_value in used_ids:
-        return jsonify({'valid': False})
-    else:
-        return jsonify({'valid': True})
+@app.route('/hello', methods=['POST'])
+def hello():
+   name = request.form.get('name')
+
+   if name:
+       print('Request for hello page received with name=%s' % name)
+       return render_template('hello.html', name = name)
+   else:
+       print('Request for hello page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
 
 
-@app.route('/success')
-def success():
-    return render_template('success.html')
-
-
-app.run()
+if __name__ == '__main__':
+   app.run()
