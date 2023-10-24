e7c58621ef9f0e6aeee370d4184c6408cd9a1d4a
MikPhD <mik.quattromini@gmail.com>
2023-10-24 16:51:50 +0200

    try

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
 
 from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+                   send_from_directory, url_for, jsonify)
 
 
 app = Flask(__name__)
-
+used_ids = ['123', '456', '789']
 @app.route('/')
 def index():
     print('Request for index page received')
@@ -16,25 +16,25 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-# @app.route('/submit', methods=['POST'])
-# def submit():
-#     input_value = request.form.get('id')
-#     print("Valore del campo di input:", input_value)
-#     return redirect(url_for('success'))
+@app.route('/submit', methods=['POST'])
+def submit():
+    input_value = request.form.get('id')
+    print("Valore del campo di input:", input_value)
+    return redirect(url_for('success'))
 #
-# @app.route('/check_id', methods=['POST'])
-# def check_id():
-#     input_value = request.form.get('id')
+@app.route('/check_id', methods=['POST'])
+def check_id():
+    input_value = request.form.get('id')
+
+    if input_value in used_ids:
+        return jsonify({'valid': False})
+    else:
+        return jsonify({'valid': True})
 #
-#     if input_value in used_ids:
-#         return jsonify({'valid': False})
-#     else:
-#         return jsonify({'valid': True})
 #
-#
-# @app.route('/success')
-# def success():
-#     return render_template('success.html')
+@app.route('/success')
+def success():
+    return render_template('success.html')
 
 @app.route('/hello', methods=['POST'])
 def hello():
